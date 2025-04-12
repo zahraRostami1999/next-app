dc4b9fcc9a5f67189a1d36a1e223b56d2dc96831
Zahra Rostami <zahrarostami137878@gmail.com>
2025-04-12 14:19:50 +0330

    feat: Implement loading state for /meals route

---
+++ app/meals/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoadingPage() {
+  return (
+    <div className='text-2xl text-yellow-400 flex justify-center my-10'>
+        Loading....
+    </div>
+  )
+}
+
+export default LoadingPage;
